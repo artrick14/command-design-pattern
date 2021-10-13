a2d0e0e91fe4e6bd613c8cee47fd2070b474974a
bjennings215 <>
2021-10-13 15:33:09 -0400

    fixed merge conflict

--- command/InputHandler.java
+++ command/InputHandler.java
@@ -13,17 +13,6 @@ public class InputHandler {
     }
 
     public boolean playCadence(int num) {
-<<<<<<< HEAD
-        if (num == 0) {
-            commands.get(0).execute();
-            return true;
-        }
-        if (num == 1) {
-            commands.get(1).execute();
-            return true;
-        }
-        if (num == 2) {
-=======
         if(num == 0) {
             commands.get(0).execute();
             return true;
@@ -33,16 +22,11 @@ public class InputHandler {
             return true;
         }
         else if(num == 2) {
->>>>>>> 8f752cc2560b1362c20a2634424cb0179655f0e3
             commands.get(2).execute();
             return true;
         }
         else {
-<<<<<<< HEAD
-         return false;
-=======
             return false;
->>>>>>> 8f752cc2560b1362c20a2634424cb0179655f0e3
         }
     }
 }
