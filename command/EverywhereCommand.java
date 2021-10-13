343eee0d656502c419c31cba494195dcfd642310
bjennings215 <>
2021-10-13 15:25:58 -0400

    add EverywhereCommand.java

--- command/EverywhereCommand.java
+++ command/EverywhereCommand.java
@@ -2,12 +2,14 @@
 
 public class EverywhereCommand implements Command {
 
+    private MilitaryCadence cadence;
+
     public EverywhereCommand(MilitaryCadence cadence) {
-       
+       this.cadence = cadence;
     }
 
     public void execute() {
-        
+        cadence.singEverywhereWeGo();
     }
     
 }
