f507bd1f188e9375cec34c529abc50d47aa73b8b
Esamsartawi1 <70250191+Esamsartawi1@users.noreply.github.com>
2021-10-13 21:42:53 -0400

    in army command

--- command/InArmyCommand.java
+++ command/InArmyCommand.java
@@ -4,17 +4,18 @@
  * @param cadence this variable is of type Military cadence and is referenced in the constructor to call the singInArmy method in execute
  */
 public class InArmyCommand implements Command {
+    private MilitaryCadence cadence;
     /**
      * This method constructs the cadence param and takes in a cadence 
      * @param cadence cadence is set as a reference to the class Military Cadence in order to call a method in this class
      */
     public InArmyCommand(MilitaryCadence cadence) {
-        
+        this.cadence=cadence;
     }
     /**
      * This method is used to call the singInArmy method using the constructed param cadence
      */
     public void execute() {
-        
+        cadence.singInArmy();
     }
 }
